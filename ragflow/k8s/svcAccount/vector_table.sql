dd31bceb39cce2e03f651acc45b9d6edd558ac27
aruneoz <arunk777@gmail.com>
2024-06-12 09:41:48 -0500

    Updated the Svc Account

--- ragflow/k8s/svcAccount/vector_table.sql
+++ ragflow/k8s/svcAccount/vector_table.sql
@@ -2,7 +2,7 @@
 
 -- DROP TABLE IF EXISTS public.neumai_vector_test;
 
-CREATE TABLE IF NOT EXISTS public.sabre_vector_synxis
+CREATE TABLE IF NOT EXISTS public.<table_name>
 (
     id character varying(100) COLLATE pg_catalog."default",
     file_id text COLLATE pg_catalog."default",
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS public.sabre_vector_synxis
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public.sabre_vector_synxis
+ALTER TABLE IF EXISTS public.<table_name>
     OWNER to postgres;
 -- Index: neumai_vector_test_embedding_idx
 
 -- DROP INDEX IF EXISTS public.neumai_vector_test_embedding_idx;
 
-CREATE INDEX IF NOT EXISTS sabre_vector_synxis_embedding_idx
+CREATE INDEX IF NOT EXISTS <table_name>_vector_<db_name>_embedding_idx
     ON public.sabre_vector_synxis USING ivfflat
     (embedding vector_cosine_ops)
     WITH (lists=100)
